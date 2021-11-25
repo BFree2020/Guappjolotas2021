8a2a0c36921861251ef153bd4c258fa2c052bc66
BFree2020 <90357122+BFree2020@users.noreply.github.com>
2021-11-24 22:29:58 -0500

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,87 @@
+const products = [
+    {
+        id: 1,
+        name: "Guajolocombo",
+        image: "..//imagenes/Guajolocombo.jpg",
+        description: "Torta de tamal con atole de sabor",
+        price: 13000
+    },
+
+    {
+        id: 2,
+        name: "Guajolota verde",
+        image: "..//imagenes/Guajolota verde.jpg",
+        description:"Torta de tamal de cerdo en salsa verde",
+        price: 12000
+    },
+
+    {
+        id: 3,
+        name: "Guajolota roja",
+        image: "..//imagenes/Guajolota roja.jpg",
+        description:"Torta de tamal de pollo en salsa roja",
+        price: 11000
+    },
+
+    {
+        id: 4,
+        name: "Guajolota rajas",
+        image: "..//imagenes/Guajolota de rajas.jpg",
+        description:"Torta de tamal de rajas con queso",
+        price: 10000
+    },
+
+    {
+        id: 5,
+        name: "Guajolota mole",
+        image: "..//imagenes/Guajolota mole.jpg",
+        description:"Torta de tamal de pollo con mole",
+        price: 11500
+    },
+
+    
+    {
+        id: 6,
+        name: "Tamales",
+        image: "..//imagenes/Tamales.jpg",
+        description:"Torta de tamal de pollo con mole",
+        price: 5000
+    },
+
+    {
+        id: 7,
+        name: "Atole de arroz con leche",
+        image: "..//imagenes/Atole de arroz con leche.jpg",
+        description:"Torta de tamal de pollo con mole",
+        price: 4000
+    },
+
+    {
+        id: 8,
+        name: "Atole de arroz",
+        image: "..//imagenes/Atole de arroz.jpg",
+        description:"Torta de tamal de pollo con mole",
+        price: 3500
+    },
+
+    {
+        id: 9,
+        name: "Atole de elote",
+        image: "..//imagenes/Atole de elote.jpg",
+        description:"Torta de tamal de pollo con mole",
+        price: 4500
+    },
+
+    {
+        id: 10,
+        name: "Atole de guayaba",
+        image: "..//imagenes/Atole de Guayaba.jpg",
+        description:"Torta de tamal de pollo con mole",
+        price: 4500
+    },
+
+
+];
+
+export default products
+
